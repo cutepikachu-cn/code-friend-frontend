42340bc94abc1a141a595a9a5edcea43bbdb10e1
2894462606 <2894462606@qq.com>
2024-01-10 21:25:37 +0800

    修改用户卡片标签显示方式，最多显示4个标签

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -9,24 +9,25 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-    <van-card
-        v-for="user in userList"
-        :key="user.id"
-        :desc="user.profile ? user.profile : '无介绍'"
-        :title="user.nickname ? user.nickname : '匿名用户'"
-    >
-      <template #thumb>
-        <van-image :src="user.avatarUrl" round/>
-      </template>
-      <template #tags>
-        <van-tag :style="{margin: '5px'}" size="medium" plain type="danger" v-for="tag in user.tags">
-          {{ tag }}
-        </van-tag>
-      </template>
-      <template #footer>
-        <van-button type="primary" size="small" round>联系伙伴</van-button>
-      </template>
-    </van-card>
+  <van-card
+      v-for="user in userList"
+      :key="user.id"
+      :desc="user.profile ? user.profile : '无介绍'"
+      :title="user.nickname ? user.nickname : '匿名用户'"
+  >
+    <template #thumb>
+      <van-image :src="user.avatarUrl" round/>
+    </template>
+    <template #tags>
+      <van-tag :style="{margin: '5px'}" size="medium" plain type="primary" v-for="tag in user.tags?.slice(0, 4)">
+        {{ tag }}
+      </van-tag>
+      {{user.tags.length > 4 ? "..." : ""}}
+    </template>
+    <template #footer>
+      <van-button type="primary" size="small" round>联系伙伴</van-button>
+    </template>
+  </van-card>
 </template>
 
 <style scoped>
